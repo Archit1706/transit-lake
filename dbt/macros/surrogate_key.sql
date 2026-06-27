2f4d0d3e91d66cdcc25114bd01345302c2b9e0dc
Archit1706 <architrathod77@gmail.com>
2026-06-26 20:22:28 -0500

    style(dbt): apply sqlfluff formatting to surrogate_key.sql

--- dbt/macros/surrogate_key.sql
+++ dbt/macros/surrogate_key.sql
@@ -3,9 +3,9 @@
    external package dependency. #}
 {% macro surrogate_key(columns) %}
     md5(
-        {%- for col in columns %}
+{%- for col in columns %}
         coalesce(cast({{ col }} as varchar), '_null_')
-        {%- if not loop.last %} || '||' || {% endif %}
-        {%- endfor %}
+{%- if not loop.last %} || '||' || {% endif %}
+{%- endfor %}
     )
 {% endmacro %}
