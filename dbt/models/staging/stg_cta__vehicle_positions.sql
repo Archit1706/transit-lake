2f1df2a2a0eccce9c347a216989e2680198c7d15
Archit1706 <architrathod77@gmail.com>
2026-06-26 19:44:18 -0500

    feat(dbt): add stg_cta__vehicle_positions.sql

---
+++ dbt/models/staging/stg_cta__vehicle_positions.sql
@@ -0,0 +1,22 @@
+with source as (
+    select * from {{ source('silver', 'silver_vehicle_positions') }}
+)
+
+select
+    {{ surrogate_key(['mode', 'vehicle_id', 'report_ts']) }} as position_sk,
+    agency,
+    mode,
+    vehicle_id,
+    route_id,
+    headsign,
+    lat,
+    lon,
+    heading,
+    is_delayed,
+    report_ts,
+    cast(report_ts as date) as report_date,
+    extract(hour from report_ts) as report_hour,
+    _ingested_at
+from source
+where lat is not null
+  and lon is not null
