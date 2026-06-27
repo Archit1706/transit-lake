18893bf467c4c6f9f00bdf58afe3a2e3e5aadb2a
Archit1706 <architrathod77@gmail.com>
2026-06-26 19:32:16 -0500

    feat(dagster): register silver assets in definitions

--- dagster_proj/definitions.py
+++ dagster_proj/definitions.py
@@ -1,7 +1,7 @@
 """Dagster code location entrypoint: collect assets, resources, schedules, sensors."""
 import dagster as dg
 
-from dagster_proj.assets import bronze
+from dagster_proj.assets import bronze, silver
 from dagster_proj.resources import duckdb_resource
 from dagster_proj.schedules import (
     daily_static_job,
@@ -11,9 +11,10 @@ from dagster_proj.schedules import (
 )
 
 bronze_assets = dg.load_assets_from_modules([bronze])
+silver_assets = dg.load_assets_from_modules([silver])
 
 defs = dg.Definitions(
-    assets=bronze_assets,
+    assets=[*bronze_assets, *silver_assets],
     jobs=[rt_poll_job, daily_static_job],
     schedules=[rt_poll_schedule, daily_static_schedule],
     resources={
