92792fa53e1a23ffb2981813690bec16f924239f
Archit1706 <architrathod77@gmail.com>
2026-06-26 20:41:41 -0500

    feat(dagster): register dbt assets and resource in definitions

--- dagster_proj/definitions.py
+++ dagster_proj/definitions.py
@@ -3,6 +3,7 @@ import dagster as dg
 
 from dagster_proj import checks
 from dagster_proj.assets import bronze, silver
+from dagster_proj.dbt import dbt_models, dbt_resource
 from dagster_proj.resources import duckdb_resource
 from dagster_proj.schedules import (
     daily_static_job,
@@ -16,11 +17,12 @@ silver_assets = dg.load_assets_from_modules([silver])
 asset_checks = dg.load_asset_checks_from_modules([checks])
 
 defs = dg.Definitions(
-    assets=[*bronze_assets, *silver_assets],
+    assets=[*bronze_assets, *silver_assets, dbt_models],
     asset_checks=asset_checks,
     jobs=[rt_poll_job, daily_static_job],
     schedules=[rt_poll_schedule, daily_static_schedule],
     resources={
         "duckdb": duckdb_resource,
+        "dbt": dbt_resource,
     },
 )
