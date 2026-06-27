8fcf5ca134c55fb79faf584f2187b4e6bfde1eca
Archit1706 <architrathod77@gmail.com>
2026-06-26 20:12:07 -0500

    feat(dagster): register asset checks in definitions

--- dagster_proj/definitions.py
+++ dagster_proj/definitions.py
@@ -1,6 +1,7 @@
 """Dagster code location entrypoint: collect assets, resources, schedules, sensors."""
 import dagster as dg
 
+from dagster_proj import checks
 from dagster_proj.assets import bronze, silver
 from dagster_proj.resources import duckdb_resource
 from dagster_proj.schedules import (
@@ -12,9 +13,11 @@ from dagster_proj.schedules import (
 
 bronze_assets = dg.load_assets_from_modules([bronze])
 silver_assets = dg.load_assets_from_modules([silver])
+asset_checks = dg.load_asset_checks_from_modules([checks])
 
 defs = dg.Definitions(
     assets=[*bronze_assets, *silver_assets],
+    asset_checks=asset_checks,
     jobs=[rt_poll_job, daily_static_job],
     schedules=[rt_poll_schedule, daily_static_schedule],
     resources={
