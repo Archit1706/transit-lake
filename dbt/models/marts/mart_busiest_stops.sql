0864f3684b81ffdbb1554df040ddd65acca0566d
Archit1706 <architrathod77@gmail.com>
2026-06-26 19:44:31 -0500

    feat(dbt): add mart_busiest_stops.sql

---
+++ dbt/models/marts/mart_busiest_stops.sql
@@ -0,0 +1,20 @@
+-- Busiest stops by scheduled departures (from the static timetable).
+with stop_times as (
+    select * from {{ ref('stg_cta__stop_times') }}
+),
+
+stops as (
+    select * from {{ ref('dim_stop') }}
+)
+
+select
+    s.stop_id,
+    s.stop_name,
+    s.stop_lat,
+    s.stop_lon,
+    count(*) as scheduled_departures,
+    count(distinct st.trip_id) as distinct_trips
+from stop_times st
+join stops s using (stop_id)
+group by 1, 2, 3, 4
+order by scheduled_departures desc
